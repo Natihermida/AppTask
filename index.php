b4d487265eea889c885862e67b39b0a522d45d79
Tonitf <subcero00@hotmail.com>
2025-02-04 19:43:57 +0100

    add partials

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejemplo de Página Web</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+
+<body>
+    <header>
+        <h1>AppTask</h1>
+        <img src="img/tareas.jpg" alt="logo Task">
+    </header>
+    <nav>
+        <a href="#">Inicio</a>
+        <a href="#">Registro</a>
+        <a href="#">Login</a>
+    </nav>
+    <section>
+        <main>
+            <h2>¡Bienvenido a AppTask!</h2>
+            <p>AppTask es una aplicación web que te permite organizar tus tareas de una forma sencilla y eficiente.</p>
+            <p>Con AppTask podrás:</p>
+            <ul>
+                <li>Registrar tus tareas.</li>
+                <li>Marcar tus tareas como completadas.</li>
+                <li>Eliminar tus tareas.</li>
+            </ul>
+            <p>¡Regístrate y comienza a organizar tus tareas con AppTask!</p>
+            
+        </main>
+    </section>
+    <footer>
+        <p>© 2025 AppTask. Todos los derechos reservados.</p>
+    </footer>
+</body>
+
+</html>
