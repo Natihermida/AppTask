fbcb10f6a45c7cd58c072ecb6448221fc2c64cc1
Tonitf <subcero00@hotmail.com>
2025-02-05 17:26:43 +0100

    unir

--- nueva_tarea.php
+++ nueva_tarea.php
@@ -2,25 +2,28 @@
 // if(isset($_POST["fecha"])){
     // session_start();
     include("conexiondb.php");
-    $idusuario = $_SESSION['idusuario'];
+    $idusuario = 2;//$_SESSION['idusuario'];
     $titulo= $_POST["titulo"];
     $descripcion = $_POST["descripcion"];
-    $fecha = $_POST["fechaInicio"];
-    $fechaFin = $_POST["fechaFin"];
+    $fecha = $_POST["fecha_inicio"];
+    $fechaFin = $_POST["fecha_fin"];
+    $estado = $_POST["estado"];
     
     
-    $sql = "INSERT INTO tareas (usuarios_id,titulo,descripcion,fecha_inicio,fecha_fin) VALUES (:usuarios_id,:titulo,:descripcion,:fecha_inicio,:fecha_fin,)";
+    $sql = "INSERT INTO tareas (usuarios_id,titulo,descripcion,fecha_inicio,fecha_fin,estado) VALUES (:usuarios_id,:titulo,:descripcion,:fecha_inicio,:fecha_fin,:estado)";
     $stmt = $conexion->prepare($sql);
     $stmt->bindParam(":usuarios_id", $idusuario);
     $stmt->bindParam(":titulo", $titulo);
     $stmt->bindParam(":descripcion", $descripcion);
     $stmt->bindParam(":fecha_inicio", $fecha);
     $stmt->bindParam(":fecha_fin", $fechaFin);
+    $stmt->bindParam(":estado", $estado);
     $stmt->execute();
     $conexion = null;
     header("Location: task.php");
+    /*
 }else{
     header("Location: task.php");
 }
-
+*/
 ?>
--- task.php
+++ task.php
@@ -8,57 +8,58 @@ $sql = "select * from tareas";
 $result = $conexion->query($sql);
 include("./partials/cabecera.php");
 ?>
-<section>
-<h3>Tareas</h3>
-            <div class="tareas">
-                <form action="nueva_tarea.php" method="post" id="formTareas">
-                <label for="titulo">Título</label>
-                <input required type="text" name="titulo" id="titulo">
-                    <label for="descripcion">Descripcion</label>
-                    <input required type="text" name="descripcion" id="descripcion">
-                    <label for="fecha_inicio">Fecha Inicio</label>
-                    <input type="date" name="fecha_inicio" id="fecha" value="">
-                    <label for="fecha_fin">Fecha Fin</label>
-                    <input type="date" name="fecha_fin" id="fecha" value="">
-                    
-                    <button>Enviar</button>
-                </form>
-            </div>
-            <div class="lista">
-                <table id="tablaTareas">
-                    <thead>
-                        <th>Id</th>
-                        <th>Fecha</th>
-                        <th>Descripcion</th>
-                        <th>Fecha Inicio</th>
-                        <th>Fecha Fin</th>
-                        <th>Estado</th>
-                        <th>Operaciones</th>
-                    </thead>
-                    <tbody id="tbodyTareas">
-                        <?php
-                        while ($row = $result->fetch()) {
-                            echo "<tr>
-                            <td>".$row['tareas_id']."</td>
-                            <td>".$row['titulo']."</td>
-                            <td>".$row['descripcion']."</td>
-                            <td>".$row['fecha_inicio']."</td>
-                            <td>".$row['fecha_fin']."</td>
-                            <td>".$row['estado']."</td>
-                                                      <td>
-                                <a href='borrar_tareas.php?tareas=".$row['id']."'><i class='fa-solid fa-trash'></i></a>
-                                <a href='editar_tareas.php?tareas=".$row['id']."'<i class='fa-solid fa-pen-to-square'></i></a>
-                            </td>
-                            </tr>";
+<section class="containerTask">
+    <h3>Tareas</h3>
+    <div class="contenedordivs">
+    <div class="tareas">
+        <form action="nueva_tarea.php" method="post" id="formTareas">
+            <label for="titulo">Título</label>
+            <input required type="text" name="titulo" id="titulo">
+            <label for="descripcion">Descripcion</label>
+            <input required type="text" name="descripcion" id="descripcion">
+            <label for="fecha_inicio">Fecha Inicio</label>
+            <input type="date" name="fecha_inicio" id="fecha" value="">
+            <label for="fecha_fin">Fecha Fin</label>
+            <input type="date" name="fecha_fin" id="fecha" value="">
+            <select name="estado" id="estado">
+                <option value="pendiente">Pendiente</option>
+                <option value="en proceso">En proceso</option>
+                <option value="finalizado">Finalizado</option>
+            </select>
+            <button>Enviar</button>
+        </form>
+    </div>
+    <div class="lista">
+        <table id="tablaTareas">
+            <thead>
+                <th>Id</th>
+                <th>Fecha</th>
+                <th>Descripcion</th>
+                <th>Fecha Inicio</th>
+                <th>Fecha Fin</th>
+                <th>Estado</th>
+                <th>Operaciones</th>
+            </thead>
+            <tbody id="tbodyTareas">
+                <?php
+                while ($row = $result->fetch()) {
+                    echo "<tr>
+                            <td>" . $row['tareas_id'] . "</td>
+                            <td>" . $row['titulo'] . "</td>
+                            <td>" . $row['descripcion'] . "</td>
+                            <td>" . $row['fecha_inicio'] . "</td>
+                            <td>" . $row['fecha_fin'] . "</td>
+                            <td>" . $row['estado'] . "
+                            </td>                            </tr>";
+                }
+                ?>
 
-                        }
-                        ?>
-                        
 
 
-                </table>
-            </div>
-        </section>
+        </table>
+    </div>
+    </div>
+</section>
 
 
 <?php
