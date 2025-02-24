21b04016495fa6aee0a762c0743056252fd32010
NatiHermida <hermidaltamarnati@gmail.com>
2025-02-24 19:41:45 +0100

    contraseñadb modificada

--- conexiondb.php
+++ conexiondb.php
@@ -1,7 +1,7 @@
 <?php
 $host = "localhost";
 $user = "root";
-$password = "123456";
+$password = "";
 $database = "apptask";
 try {
     $conexion = new PDO("mysql:host=$host;dbname=$database", $user, $password);
